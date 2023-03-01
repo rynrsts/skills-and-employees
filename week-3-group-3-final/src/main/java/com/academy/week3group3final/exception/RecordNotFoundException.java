9bc0097a10800833d3c92220b41084f12fe1fed6
ryan-aristosa <ryan.aristosa@softvision.com>
2023-03-01 17:29:05 +0800

    Remove project folder

--- week-3-group-3-final/src/main/java/com/academy/week3group3final/exception/RecordNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.academy.week3group3final.exception;
-
-public class RecordNotFoundException extends RuntimeException {
-
-    private static final long serialVersionUID = 1L;
-
-}
--- week-3-group-3-final/src/main/java/com/academy/week3group3final/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.academy.week3group3final.repository;
-
-import com.academy.week3group3final.model.Employee;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
-}
